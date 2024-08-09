2e6933120d177fbba50ace83aba946749bf0acd3
”ukrWily” <ukrwily@gmail.com>
2024-08-09 21:04:22 +0300

    PC first

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <form action="">
+      <label htmlFor="">
+        Username: <input type="text" name="username" id="" />
+      </label>
+      <label htmlFor="">
+        Password: <input type="password" name="password" id="" />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default Login;
